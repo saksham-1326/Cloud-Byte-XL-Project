d968a38593e21bf19cdf1d07febe4ea4af2eaf3f
Saksham <saksham.132611@gmail.com>
2026-08-10 23:28:05 +0530

    Test webhook integration

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
     <div style={{ padding: "40px", fontFamily: "Arial" }}>
       <h1>Cloud-ByteXL</h1>
 
-      <p>Frontend is running successfully.</p>
+      <p>Frontend is running successfully.Yeaa Nigga!</p>
 
       <button onClick={testBackend} disabled={loading}>
         {loading ? "Connecting..." : "Test Backend"}
